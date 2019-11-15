cca05d59a1c41a9b54a0a1193e6d06c1321e8ac0
alexTAD <alex.vialar@gmail.com>
2019-11-15 14:44:35 +0100

    Debut v.final

---
+++ Objets/Personnage.py
@@ -0,0 +1,33 @@
+from socket import *
+
+
+class Personnage(object):
+
+    def __init__(self, pseudo, serveur, connexion):
+        self.estVivant = True
+        self.estProtege = False
+        self.estMaire = False
+        self.accesChat = 1
+        self.pseudo = pseudo
+        self.connexion = serveur
+
+    def tuer(self):
+        valide = False
+        if self.estProtege != True:
+            valide = True
+        return valide
+
+    def meurt(self):
+        self.communique([])
+
+    def vote(self, pseudo):
+        pouvoir = 1
+        if self.estMaire == True:
+            pouvoir = 2
+        self.communique([self.pseudo, "vote", pseudo, str(pouvoir)])
+
+    def communique(self, instructions):
+        commande = instructions[0]
+        for i in range(1, len(instructions)):
+            commande = commande + " " + instructions[i]
+        self.connexion.send(commande.encode('Utf-8'))
